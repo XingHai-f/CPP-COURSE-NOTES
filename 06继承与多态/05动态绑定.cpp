64dc29f0f4c2f9ce1fbc470fb58cefcd401b5335
XingHai-f <124681986+XingHai-f@users.noreply.github.com>
2024-06-09 10:25:07 +0800

    Add files via upload

---
+++ 06继承与多态/05动态绑定.cpp
@@ -0,0 +1,47 @@
+/*
+虚函数和动态绑定
+问题：是不是虚函数的调用一定是动态绑定？ 肯定不是
+在类的构造函数当中，调用虚函数，也是静态绑定（构造函数中调用其它函数（虚），不会发生动态绑定）
+*/
+
+#include <iostream>
+using namespace std;
+
+class Base {
+public:
+	Base(int data = 0) :mb(data) { cout << "Base()" << endl; }
+	~Base() { cout << "~Base()" << endl; }
+	virtual void show() { cout << "Base::show()" << endl; }
+protected:
+	int mb;
+};
+
+class Derive:public Base {
+public:
+	Derive(int data = 0) :Base(data), md(data) { cout << "Derive()" << endl; }
+	~Derive() { cout << "~Derive()" << endl; }
+	void show() { cout << "Derive::show()" << endl; }
+private:
+	int md;
+};
+
+int main() {
+	Base b;
+	Derive d;
+
+	// 静态绑定，用对象本身调用虚函数，是静态绑定
+	b.show(); // 虚函数  call Base::show()
+	d.show(); // 虚函数  call Base::show()
+
+	// 动态绑定（必须由指针调用虚函数） 
+	Base* pb1 = &b;
+	pb1->show();
+	Base* pb2 = &d;
+	pb2->show();
+
+	Base& rb1 = b;
+	rb1.show();
+	Base& rb2 = d;
+	rb2.show();
+	return 0;
+}
